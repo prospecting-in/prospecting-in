65b8d2db2feee6e16d71a3007c96dc2189ad97de
Jesse <jesse@localhost.localdomain>
2019-11-29 06:49:24 +0700

    cc

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -89,9 +89,63 @@ $(document)
         }
         return false;
       });
-    //reset previously set border colors and hide all message on .keyup()
-    //$("#contact_form input, #contact_form textarea").keyup(function(){
-    //    $("#contact_form input, #contact_form textarea").css('border-color', '');
-    //    $("#result").slideUp();
-    //});
+
+
+
+      $("#submit_btn2")
+      .click(function () {
+        //get input field values
+        var user_name = $('input[name=name2]')
+          .val();
+        var user_email = $('input[name=email2]')
+          .val();
+        var user_message = $('textarea[name=message2]')
+          .val();
+        var choice = $('input[name=choice2]')
+          .val();
+        var ref = getCookie('ref');
+        if(ref) {
+          choice = choice + " : ref-cookie = " + ref;
+        }
+
+        var proceed = true;
+        if(user_name == "") {
+          $('input[name=name2]')
+            .css('border-color', '#e41919');
+          proceed = false;
+        }
+        if(user_email == "") {
+          $('input[name=email2]')
+            .css('border-color', '#e41919');
+          proceed = false;
+        }
+        if(user_message == "") {
+          $('textarea[name=message2]')
+            .css('border-color', '#e41919');
+          //    proceed = false;
+        }
+        //everything looks good! proceed...
+        if(proceed) {
+          //data to be sent to server
+          post_data = {
+            'name': user_name,
+            'email': user_email,
+            'message': user_message,
+            'choice': choice
+          };
+          var url = 'https://script.google.com/macros/s/AKfycbxk_Np2R_F1LlinTA33bAQPyg5XZn-rrs4M5xWC3lGRvts2kSX_/exec';
+          var jqxhr = $.post(url, post_data, function (post_data) {
+              setTimeout(function () {
+                window.location.replace('thank-you.html');
+              }, 500);
+            })
+            .fail(function (data) {
+              setTimeout(function () {
+                window.location.replace('error.html');
+              }, 500);
+            });
+        }
+        return false;
+      });
+
   });
